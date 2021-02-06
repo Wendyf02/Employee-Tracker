b52ee032cdd7bac0d71e226a329408d4834c35a4
Wendy Fgigueroa <camomila444@gmail>
2021-02-05 20:52:08 -0500

    working on app page

--- app.js
+++ app.js
@@ -13,33 +13,71 @@ const connection = mysql.createConnection({
     user: "root",
   
     // Your password
-    password: "",
+    password: "Liebeberlin",
     database: "employee_trackerDB"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as Id" + connection.threadId)
-    // startPrompt();
+    startPrompt();
   });
   
+function startPrompt() {
+     inquirer.prompt([
+
+    {
+     type: "list",
+     message: "What would you like to do?",
+     name: "choice",
+     choices: [
+               "View All Employees",
+               "View All Employee's by Role",
+               "View All Employees by Deparments",
+               "Update Employee",
+               "Add Employee",
+               "Add Role",
+               "Add Department"
+             ]
+    }
+
+]).then(function(answer) {
+    switch (answer.action) {
+
+      case "View all Employee by Department":
+           viewAllDepartment();
+
+      case "View All Employee by Role":
+           viewAllEmployeeRole();
+           
+      case  "Add Employee":
+            addEmployee();     
+       
+
+      case   "View All Employee":
+              viewAllEmployee();
 
 
+      case  "Updated Employee":
+               updateEmployee();
+      
+               
+      case    "Add Role":
+                 addRole();
+                 
+      case     "Add Department":
+                 addDepartment();           
+
+     }
+ })
 
 
-
-
-
-
-
-
-
-
-
+}
+//  console.log(startPrompt)
 
 //----View by Department----//
 
-  function selectDepartment() {
+  function viewDepartment() {
     var query = "SELECT * FROM department";
     connection.query(query, function(err, res) {
       console.table(res) 
@@ -49,7 +87,7 @@ connection.connect(function(err) {
 
 //-----Select Role-----//
 
- function selectRole() {
+ function viewRole() {
     var query = "SELECT * FROM role";
     connection.query(query, function(err, res) {
       console.table(res) 
