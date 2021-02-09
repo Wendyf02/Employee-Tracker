e90d88219a9ee7fe74b80b69f141b983c40b1581
Wendy Fgigueroa <camomila444@gmail>
2021-02-09 18:40:39 -0500

    updating readme page

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Liebeberlin",
+  password: "",
   database: "employee_trackerDB"
 
 });
